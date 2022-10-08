d46cf9d9736cc3f9f30c59704f96bde22c48f795
maizurzu <101496219+maizurzu@users.noreply.github.com>
2022-10-08 17:57:21 +0900

    day1
    react 시작
    리액트 구성
    Props

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Profile from './profile.js'
+
+
+function App() {
+
+  return (
+    <>
+  <Profile name='미미' role='랩퍼' team ='오마이걸'/>
+  <Profile name='제니' role='보컬' team ='블랙핑크'/>
+  <Profile name='안유징' role='보컬' team ='아이브'/>
+    </>
+  );
+}
+
+
+export default App;
